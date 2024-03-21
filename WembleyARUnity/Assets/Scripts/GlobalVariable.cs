40d3bed49a1ecd98c2797f6fb0d934c810cd31c9
Nguyễn Trọng Khang <khang.nguyentrong.sistrain@gmail.com>
2024-03-21 23:24:26 +0700

    commit

--- WembleyARUnity/Assets/Scripts/GlobalVariable.cs
+++ WembleyARUnity/Assets/Scripts/GlobalVariable.cs
@@ -1,10 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class GlobalVariable : MonoBehaviour
 {
-    public static bool task1; 
-    public static bool task2;
-    public static bool[] outputStation1;
+    public static int encoderPosition = 0;
+    //? Station 1
+    public static bool[] inputStation1 = new bool[14];
+    public static bool[] outputStation1 = new bool[32];
+
 }
--- WembleyARUnity/Assets/Scripts/Station/Station1/IOList.cs
+++ WembleyARUnity/Assets/Scripts/Station/Station1/IOList.cs
@@ -9,16 +9,30 @@ public class IOList : MonoBehaviour
     public GameObject inputBtn, outputBtn;
     public Sprite inputBtnOn, inputBtnOff;
     public Sprite outputBtnOn, outputBtnOff;
-
+    public GameObject[] inputCheck = new GameObject[14];
+    public GameObject[] outputCheck = new GameObject[32];
+   
+    
     // Start is called before the first frame update
     void Start()
     {
         inputBtn.GetComponent<Image>().sprite = inputBtnOn;
         outputBtn.GetComponent<Image>().sprite = outputBtnOff;
-
-
         contentInput.SetActive(true);
         contentOutput.SetActive(false);
+    }
+    void Update()
+    {
+        for (var i = 0; i < GlobalVariable.inputStation1.Length; i++)
+        {           
+            if (GlobalVariable.inputStation1[i]) inputCheck[i].SetActive(true);
+            else inputCheck[i].SetActive(false);
+        }
+        for (var i = 0; i < GlobalVariable.outputStation1.Length; i++)
+        {
+            if (GlobalVariable.outputStation1[i]) outputCheck[i].SetActive(true);
+            else outputCheck[i].SetActive(false);
+        }
     }
     public void onClickInputBtn()
     {
